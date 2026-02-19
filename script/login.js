462b014e003ed5bc85a279374d90666778c0bac2
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-02-19 14:23:04 +0600

    add a machine js to make the function easy

--- script/login.js
+++ script/login.js
@@ -2,14 +2,15 @@ document.getElementById("login-btn").addEventListener("click", function () {
   const phoneNumber = document.getElementById("phone-number");
   const contactNumber = phoneNumber.value;
   const pinNumber = document.getElementById("input-pin");
-    const pin = pinNumber.value;
-    localStorage.setItem("userPin", pin);
-    if (contactNumber.length === 11 && pin.length === 4) {
-        alert("Congratulation, Log in Successful");
-        window.location.assign("../home.html");
-    }
-    else {
-        alert("Log in failed, Please, check your 11 digit phone number and 4 digit pin"); 
-        return;
-    }
+  const pin = pinNumber.value;
+  localStorage.setItem("userPin", pin);
+  if (contactNumber.length === 11 && pin.length === 4) {
+    alert("Congratulation, Log in Successful");
+    window.location.assign("../home.html");
+  } else {
+    alert(
+      "Log in failed, Please, check your 11 digit phone number and 4 digit pin",
+    );
+    return;
+  }
 });
---
+++ script/machine.js
@@ -0,0 +1,4 @@
+function getValueFromInput(id) {
+    const input = document.getElementById(id);
+    const value = input.value;
+}
